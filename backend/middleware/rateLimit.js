11d36d1e5ec8849fb19a34e7340b2d0151e4739d
Krishn Jitanshu <jitanshukrishn@gmail.com>
2025-09-04 05:14:33 +0000

    Update rateLimit.js

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,9 +1,8 @@
-// backend/middleware/rateLimit.js
 const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
-  windowMs: 60 * 1000,  // 1 minute
-  max: 5,               // 5 requests per minute
+  windowMs: 60 * 1000,  
+  max: 5,               
   standardHeaders: true,
   legacyHeaders: false,
   message: { error: "Too many requests, try again later." },
